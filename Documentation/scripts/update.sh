f1259ed89d8f6610674a62844084787b3a5d2062
Andrian Nord <NightNord@gmail.com>
2010-05-19 23:29:42 +0400

    update.sh: implement auto-updating script
    Based on evadim's UPDATE instructions file

---
+++ Documentation/scripts/update.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+PORTDIR="$(portageq portdir)"
+REPO_PATH="$(portageq get_repo_path / enlightenment-niifaq)"
+
+portage_updates_dir="${PORTDIR}/profiles/updates"
+
+if ! [[ -w "${portage_updates_dir}" ]]; then
+	echo "You need to have access to PORTDIR"
+fi
+
+if [[ -z "${REPO_PATH}" ]]; then
+	echo "enlightenment-niifaq overlay not found"
+	echo "You should enable it in your make.conf"
+fi
+
+for update in "${REPO_PATH}"/Documentation/updates/*; do 
+	cat "${update}" >> "${portage_update_dir}/$(basename ${update})"
+done
+
+emerge --metadata

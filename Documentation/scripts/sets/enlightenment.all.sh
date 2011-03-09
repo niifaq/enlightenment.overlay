b2a4dccb2e98c05edb930f78ba45425e6bdc82a6
Andrian Nord <NightNord@gmail.com>
2011-03-10 01:22:52 +0300

    fix lightmediascanner conflict with @enlightenment-all
    Signed-off-by: Andrian Nord <NightNord@gmail.com>

--- Documentation/scripts/sets/enlightenment.all.sh
+++ Documentation/scripts/sets/enlightenment.all.sh
@@ -5,6 +5,6 @@ REPO_ROOT="$(portageq get_repo_path ${ROOT} enlightenment-niifaq)"
 
 BROKEN="${REPO_ROOT}/sets/enlightenment-broken"
 
-for ebuild in $(find "${REPO_ROOT}" -name "*-9999.ebuild" | sed -r "s|^.*/([[:alnum:]_\.\-]+)/[^\/]+/([[:alnum:]_\.\-]+)\.ebuild|=\1/\2|"); do
+for ebuild in $(find "${REPO_ROOT}" -name "*-9999.ebuild" | sed -r "s|^.*/([[:alnum:]_\.\-]+)/[^\/]+/([[:alnum:]_\.\-]+)-9999\.ebuild|\1/\2|"); do
 	grep -q "${ebuild}" "${BROKEN}" || echo ${ebuild};
 done;

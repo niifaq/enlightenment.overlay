c9cbc82c65480716471f53e55c94304deca60f81
Andrian Nord <NightNord@gmail.com>
2012-03-19 01:16:21 +0400

    sets/enlightenment-all: add enlightenment.ignore file check
    Thanks to Oliver Roeschke and Mellon (on e17@c.g.r) for idea.

--- Documentation/scripts/sets/enlightenment.all.sh
+++ Documentation/scripts/sets/enlightenment.all.sh
@@ -6,5 +6,11 @@ REPO_ROOT="$(portageq get_repo_path ${ROOT} enlightenment-niifaq)"
 BROKEN="${REPO_ROOT}/sets/enlightenment-broken"
 
 for ebuild in $(find "${REPO_ROOT}" -name "*-9999.ebuild" | sed -r "s|^.*/([[:alnum:]_\.\-]+)/[^\/]+/([[:alnum:]_\.\-]+)-9999\.ebuild|\1/\2|"); do
-	grep -q "${ebuild}" "${BROKEN}" || echo ${ebuild};
+	grep -q "${ebuild}" "${BROKEN}" && continue;
+
+	test -f /etc/portage/enlightenment.ignore &&
+		grep -q "^${ebuild}$" /etc/portage/enlightenment.ignore &&
+		continue;
+
+	echo ${ebuild};
 done;
